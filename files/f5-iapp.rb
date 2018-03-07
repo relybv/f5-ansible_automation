64312c37ed67138e441e552ff43c1ad57df9a747
P. Gomersbach <p.gomersbach@rely.nl>
2018-03-07 15:38:48 +0000

    add params from json

--- files/f5-iapp.rb
+++ files/f5-iapp.rb
@@ -1,8 +1,10 @@
 require 'optparse'
 require 'selenium-webdriver'
+require 'json'
 require 'rspec/expectations'
 include RSpec::Matchers
 
+template = "f5.citrix_vdi.v2.4.2"
 options = {:lbuser => nil, :Lbpasswd => nil}
 
 parser = OptionParser.new do|opts|
@@ -23,6 +25,7 @@ end
 
 parser.parse!
 
+# check for required options
 if options[:lbuser] == nil
         puts 'Please use --lbuser lbuser parameter'
         exit
@@ -33,14 +36,73 @@ if options[:lbpasswd] == nil
         exit
 end
 
+# check for stdin (example usage: cat <jsonfile> | ruby f5-iapp.rb 
+str = (STDIN.tty?) ? 'not reading from stdin' : $stdin.read
+if str == 'not reading from stdin'
+        puts 'Please use stdin to pass json'
+        exit
+end
+
+# parse stdin as json
+parsed = JSON.parse(str)
+
+# get lb names and ip addresses
+loadbalancernames = []
+loadbalancerips = []
+parsed.each do |key, array|
+  if "#{key}" != "all"
+    array.each do |key2, array2|
+      if array2[0]
+        loadbalancerips.push( array2[0] )
+        loadbalancernames.push( key )
+      end
+    end
+  end
+end
+loadbalancerip = loadbalancerips[0]
+
+# extract all vars for easy reference
+cert_name = parsed["all"]["vars"]["cert_name"]
+apm__active_directory_password = parsed["all"]["vars"]["apm__active_directory_password"]
+apm__active_directory_server = parsed["all"]["vars"]["apm__active_directory_server"]
+apm__active_directory_username = parsed["all"]["vars"]["apm__active_directory_username"]
+apm__ad_password = parsed["all"]["vars"]["apm__ad_password"]
+apm__ad_tree = parsed["all"]["vars"]["apm__ad_tree"]
+apm__ad_user = parsed["all"]["vars"]["apm__ad_user"]
+apm__login_domain = parsed["all"]["vars"]["apm__login_domain"]
+button = parsed["all"]["vars"]["button"]
+button_end = parsed["all"]["vars"]["button_end"]
+button_start = parsed["all"]["vars"]["button_start"]
+general__domain_name = parsed["all"]["vars"]["general__domain_name"]
+ha = parsed["all"]["vars"]["ha"]
+ipprefix = parsed["all"]["vars"]["ipprefix"]
+klantnaam = parsed["all"]["vars"]["klantnaam"]
+line_color_end = parsed["all"]["vars"]["line_color_end"]
+line_color_start = parsed["all"]["vars"]["line_color_start"]
+logo = parsed["all"]["vars"]["logo"]
+nlc = parsed["all"]["vars"]["nlc"]
+partition = parsed["all"]["vars"]["partition"]
+password = parsed["all"]["vars"]["password"]
+search_bar = parsed["all"]["vars"]["search_bar"]
+user = parsed["all"]["vars"]["user"]
+vlan = parsed["all"]["vars"]["vlan"]
+webui_pool__webui_dns_name = parsed["all"]["vars"]["webui_pool__webui_dns_name"]
+webui_virtual__addr = parsed["all"]["vars"]["webui_virtual__addr"]
+webui_virtual__clientssl_profile = parsed["all"]["vars"]["webui_virtual__clientssl_profile"]
+webui_virtual__custom_uri = parsed["all"]["vars"]["webui_virtual__custom_uri"]
+welcome_text = parsed["all"]["vars"]["welcome_text"]
+xml_broker_pool__monitor_app = parsed["all"]["vars"]["xml_broker_pool__monitor_app"]
+xml_broker_pool__monitor_password = parsed["all"]["vars"]["xml_broker_pool__monitor_password"]
+xml_broker_pool__monitor_username = parsed["all"]["vars"]["xml_broker_pool__monitor_username"]
+xml_broker_virtual__addr = parsed["all"]["vars"]["xml_broker_virtual__addr"]
+
 # open chrome driver
 driver = Selenium::WebDriver.for :chrome
 driver.manage.timeouts.implicit_wait = 30
 
 # open f5 management url
-driver.get 'https://212.61.222.170/tmui/login.jsp'
-# driver.manage.window.maximize
-expect(driver.title).to eql 'BIG-IP® - ic-lb-lab-1.1.lb.nl.clara.net (10.255.0.200)'
+driver.get "https://#{loadbalancerip}/tmui/login.jsp"
+# expect(driver.title).to eql 'BIG-IP® - ic-lb-lab-1.1.lb.nl.clara.net (10.255.0.200)'
 
 # login
 element = driver.find_element :name => "username"
@@ -50,17 +112,17 @@ element.send_keys options[:lbpasswd]
 element.submit
 
 # goto application
-driver.navigate.to 'https://212.61.222.170/tmui/Control/jspmap/tmui/application/list.jsp'
+driver.navigate.to "https://#{loadbalancerip}/tmui/Control/jspmap/tmui/application/list.jsp"
 # select partition
 sleep(5)
 menu = driver.find_element(:id, 'partition_control')
 option = Selenium::WebDriver::Support::Select.new(menu)
-option.select_by(:value, 'Ansible-test')
+option.select_by(:value, "#{partition}")
 
 # check if iapp exists
 driver.switch_to.frame driver.find_element(:id, "contentframe")
 begin
-    driver.find_element(:link, "test-iapp")
+    driver.find_element(:link, "#{klantnaam}")
     puts 'deleting iapp'
     driver.find_element(:name, "checkbox1").click
     driver.find_element(:id, "delete").click
@@ -73,45 +135,43 @@ puts 'create iapp'
 driver.find_element(:name, "exit_button").click
 sleep(2)
 driver.find_element(:id, "application_name").clear
-driver.find_element(:id, "application_name").send_keys "test-iapp"
+driver.find_element(:id, "application_name").send_keys "#{klantnaam}"
 driver.find_element(:id, "template_name").click
-driver.find_element(:css, "option[value=\"/Common/f5.citrix_vdi.v2.4.2\"]").click
+driver.find_element(:css, "option[value=\"/Common/#{template}\"]").click
 sleep(5)
 driver.find_element(:id, "/tmui/application/create.jsp?.template_selection_tabletoggle")
 driver.find_element(:id, "var__general__domain_name").click
 driver.find_element(:id, "var__general__domain_name").clear
-driver.find_element(:id, "var__general__domain_name").send_keys "ADNETBIOS"
+driver.find_element(:id, "var__general__domain_name").send_keys "#{general__domain_name}"
 driver.find_element(:id, "var__apm__login_domain").click
 driver.find_element(:id, "var__apm__login_domain").clear
-driver.find_element(:id, "var__apm__login_domain").send_keys "AD.FQDN.NL"
+driver.find_element(:id, "var__apm__login_domain").send_keys "#{apm__login_domain}"
 driver.find_element(:id, "var__apm__active_directory_server__host___1").click
 driver.find_element(:id, "var__apm__active_directory_server__host___1").clear
-driver.find_element(:id, "var__apm__active_directory_server__host___1").send_keys "ad1.claranet.nl"
+driver.find_element(:id, "var__apm__active_directory_server__host___1").send_keys "#{apm__active_directory_server}"
 driver.find_element(:id, "var__apm__active_directory_server__addr___1").click
 driver.find_element(:id, "var__apm__active_directory_server__addr___1").clear
-driver.find_element(:id, "var__apm__active_directory_server__addr___1").send_keys "1.1.1.1"
+driver.find_element(:id, "var__apm__active_directory_server__addr___1").send_keys "#{apm__active_directory_server}"
 driver.find_element(:id, "var__apm__allow_anonymous_binding").click
 Selenium::WebDriver::Support::Select.new(driver.find_element(:id, "var__apm__allow_anonymous_binding")).select_by(:text, "No, credentials are required for binding")
 driver.find_element(:css, "option[value=\"credentials_required\"]").click
 driver.find_element(:id, "var__apm__active_directory_username").click
 driver.find_element(:id, "var__apm__active_directory_username").clear
-driver.find_element(:id, "var__apm__active_directory_username").send_keys "BINDUSER"
+driver.find_element(:id, "var__apm__active_directory_username").send_keys "#{apm__active_directory_username}"
 driver.find_element(:id, "var__apm__active_directory_password").click
 driver.find_element(:id, "var__apm__active_directory_password").clear
-driver.find_element(:id, "var__apm__active_directory_password").send_keys "BINDPASSWD"
+driver.find_element(:id, "var__apm__active_directory_password").send_keys "#{apm__active_directory_password}"
 driver.find_element(:id, "var__apm__ad_user").click
 driver.find_element(:id, "var__apm__ad_user").clear
-driver.find_element(:id, "var__apm__ad_user").send_keys "ADMONUSER"
+driver.find_element(:id, "var__apm__ad_user").send_keys "#{apm__ad_user}"
 driver.find_element(:id, "var__apm__ad_password").click
 driver.find_element(:id, "var__apm__ad_password").clear
-driver.find_element(:id, "var__apm__ad_password").send_keys "ADMONPASSWD"
+driver.find_element(:id, "var__apm__ad_password").send_keys "#{apm__ad_user}"
 driver.find_element(:id, "var__apm__ad_tree").click
 driver.find_element(:id, "var__apm__ad_tree").clear
-driver.find_element(:id, "var__apm__ad_tree").send_keys "ou=CLARA"
+driver.find_element(:id, "var__apm__ad_tree").send_keys "#{apm__ad_tree}"
 driver.find_element(:id, "var__webui_virtual__clientssl_profile").click
-# Selenium::WebDriver::Support::Select.new(driver.find_element(:id, "var__webui_virtual__clientssl_profile")).select_by(:text, "claranet-wildcard")
-Selenium::WebDriver::Support::Select.new(driver.find_element(:id, "var__webui_virtual__clientssl_profile")).select_by(:text, "clientssl")
-# driver.find_element(:css, "option[value=\"/Common/claranet-wildcard\"]").click
+Selenium::WebDriver::Support::Select.new(driver.find_element(:id, "var__webui_virtual__clientssl_profile")).select_by(:text, "/Common/#{cert_name}")
 driver.find_element(:css, "option[value=\"/Common/clientssl\"]").click
 driver.find_element(:id, "var__webui_virtual__addr").click
 driver.find_element(:id, "var__webui_virtual__addr").clear
